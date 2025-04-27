1efb30bc16f28f3883b253cb5c3b3fb01072c12c
Leander Christmann <l.christmann@pharma4u.de>
2025-04-27 22:07:37 +0200

    finish creating basic pages of project, fix links (4:43:05 in course)

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/car/search', [CarController::class, 'search'])->name('car.search'); // must be defined above the below line, bcs else the route car/{car} would match instead of car/search (run artisan route:list!)
+Route::resource('/car', CarController::class);
 
 Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 Route::get('/login', [LoginController::class, 'create'])->name('login');
