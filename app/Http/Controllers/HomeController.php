604e103fc3cbc15299c2642b19e3eb87a802c31c
Leander Christmann <l.christmann@pharma4u.de>
2025-05-08 15:30:53 +0200

    add Eloquent ORM factories (8:15:55 in course)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,10 @@ use App\Models\Car;
 use App\Models\CarFeatures;
 use App\Models\CarImage;
 use App\Models\CarType;
+use App\Models\Maker;
+use App\Models\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class HomeController extends Controller
 {
@@ -193,6 +196,52 @@ class HomeController extends Controller
 //        $user->favouriteCars()->detach([1, 2]);
 //        $user->favouriteCars()->detach(); // detach everything
 //        dd($user->favouriteCars);
+
+//        $maker = User::factory()
+//            ->count(10)
+//            ->state([
+//                'name' => 'Leander'
+//            ])
+//            ->create();
+
+//        User::factory()
+//            ->count(10)
+////            ->sequence(
+////                ['name' => 'Leander'],
+////                ['name' => 'Zura']
+////            )
+//            ->sequence(fn  (Sequence $sequence) => ['name' => 'Name ' . $sequence->index])
+//            ->create();
+
+//        Car::factory()->count(2)->trashed()->create(); // works only if there's a deleted_at column
+
+        // Callback methods, e.g. if after creating a user, I want to associate cars to him or something
+//        User::factory()
+//            ->afterCreating(function (User $user) {
+//                dump($user);
+//            })
+//            ->create();
+
+//        Maker::factory()
+//            ->count(1)
+////            ->hasModels(1, function (array $attributes, Maker $maker) {
+////                return [];
+////            })
+//            ->has(Model::factory()->count(3))
+//            ->create();
+
+//        $maker = Maker::factory()->state(['name' => 'Lalala'])->create();
+//        Model::factory()
+//            ->count(2)
+//            ->for($maker)
+////            ->forMaker(['name' => 'Lexus'])
+////            ->for(Maker::factory()->state(['name' => 'Lexus']))
+//            ->create();
+
+//        User::factory()
+//            ->has(Car::factory()->count(5), 'favouriteCars')
+////            ->hasAttached(Car::factory()->count(5), ['col1' => 'value1'], 'favouriteCars')
+//            ->create();
 
         return view('home.index');
     }
