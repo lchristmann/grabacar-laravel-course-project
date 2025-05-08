4c0e1a253eb8c9709104aba61e376eb87a842e02
Leander Christmann <l.christmann@pharma4u.de>
2025-05-08 16:56:24 +0200

    add database seeding (8:40:50 in course)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Car;
+use App\Models\CarImage;
+use App\Models\CarType;
+use App\Models\City;
+use App\Models\FuelType;
+use App\Models\Maker;
+use App\Models\Model;
+use App\Models\State;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +21,98 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        CarType::factory()
+            ->sequence(
+                ['name' => 'Sedan',],
+                ['name' => 'Hatchback',],
+                ['name' => 'SUV',],
+                ['name' => 'Pickup Truck',],
+                ['name' => 'Minivan',],
+                ['name' => 'Jeep',],
+                ['name' => 'Coupe',],
+                ['name' => 'Crossover',],
+                ['name' => 'Sports Car',],
+            )
+            ->count(9)
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        FuelType::factory()
+            ->sequence(
+                ['name' => 'Gasoline'],
+                ['name' => 'Diesel'],
+                ['name' => 'Electric'],
+                ['name' => 'Hybrid'],
+            )
+            ->count(4)
+            ->create();
+
+        $states = [
+            'California' => ['Los Angeles', 'San Francisco', 'San Diego'],
+            'Texas' => ['Houston', 'San Antonio', 'Dallas', 'Austin'],
+            'Florida' => ['Miami', 'Orlando', 'Tampa', 'Jacksonville'],
+            'New York' => ['New York City', 'Buffalo', 'Rochester', 'Yonkers'],
+            'Illinois' => ['Chicago', 'Aurora', 'Naperville', 'Joliet'],
+            'Pennsylvania' => ['Philadelphia', 'Pittsburgh', 'Allentown'],
+            'Ohio' => ['Columbus', 'Cleveland', 'Cincinnati', 'Toledo'],
+            'Georgia' => ['Atlanta', 'Augusta', 'Columbus', 'Savannah'],
+            'North Carolina' => ['Charlotte', 'Raleigh', 'Greensboro'],
+            'Michigan' => ['Detroit', 'Grand Rapids', 'Warren']
+        ];
+        foreach ($states as $state => $cities) {
+            State::factory()
+                ->state(['name' => $state])
+                ->has(
+                    City::factory()
+                    ->count(count($cities))
+                    ->sequence(...array_map(fn($city) => ['name' => $city], $cities))
+                )
+                ->create();
+        }
+
+        $makers = [
+            'Toyota' => ['Camry', 'Corolla', 'Highlander', 'RAV4', 'Prius'],
+            'Ford' => ['F-150', 'Escape', 'Explorer', 'Mustang', 'Fusion'],
+            'Honda' => ['Civic', 'Accord', 'CR-V', 'Pilot', 'Odyssey'],
+            'Chevrolet' => ['Silverado', 'Equinox', 'Malibu', 'Impala'],
+            'Nissan' => ['Altima', 'Sentra', 'Rogue', 'Maxima', 'Murano'],
+            'Lexus' => ['RX400', 'RX450', 'RX350', 'ES350', 'LS500']
+        ];
+        foreach ($makers as $maker => $models) {
+            Maker::factory()
+                ->state(['name' => $maker])
+                ->has(
+                    Model::factory()
+                    ->count(count($models))
+                    ->sequence(...array_map(fn($model) => ['name' => $model], $models))
+                )
+                ->create();
+        }
+
+        User::factory()
+            ->count(3)
+            ->create();
+
+        User::factory()
+            ->count(2)
+            ->has(
+                Car::factory()
+                    ->count(50)
+                    ->has(
+                        CarImage::factory()
+                            ->count(5)
+                            ->sequence(fn (Sequence $sequence) => ['position' => $sequence->index % 5 + 1]),
+//                            ->sequence(
+//                                ['position' => 1],
+//                                ['position' => 2],
+//                                ['position' => 3],
+//                                ['position' => 4],
+//                                ['position' => 5],
+//                            ),
+                        'images'
+                    )
+                    ->hasFeatures(),
+                'favouriteCars'
+            )
+            ->create();
     }
 }
